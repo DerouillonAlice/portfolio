af42f2a6f4be7e1ff5fdd1bcebfd1a35684d7c6b
DerouillonAlice <contact@alice.sh>
2025-08-11 18:11:27 +0200

    fix: update Navbar layout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex flex-wrap justify-around sm:justify-end gap-4 sm:gap-8 sm:px-5 pb-6 px-0 sm:py-3 text-white">
+  <nav class="flex flex-wrap justify-between items-center gap-4 sm:gap-8 sm:px-5 pb-6 px-0 sm:py-3 text-white">
     <router-link
       to="/"
       class="text-white font-bold no-underline hover:text-primary hover:scale-110 transition-all"
@@ -10,38 +10,41 @@
         <i class="fas fa-home"></i>
       </span>
     </router-link>
-    <router-link
-      to="/portfolio"
-      class="text-white font-bold no-underline hover:text-primary hover:scale-110 transition-all"
-      exact-active-class="text-primary"
-      v-slot="{ isActive }"
-    >
-      <span :class="{ 'text-primary': isActive }">Projets</span>
-    </router-link>
-    <router-link
-      to="/competences"
-      class="text-white font-bold no-underline hover:text-primary hover:scale-110 transition-all"
-      exact-active-class="text-primary"
-      v-slot="{ isActive }"
-    >
-      <span :class="{ 'text-primary': isActive }">Compétences</span>
-    </router-link>
-    <router-link
-      to="/cv"
-      class="text-white font-bold no-underline hover:text-primary hover:scale-110 transition-all"
-      exact-active-class="text-primary"
-      v-slot="{ isActive }"
-    >
-      <span :class="{ 'text-primary': isActive }">CV</span>
-    </router-link>
-    <router-link
-      to="/contact"
-      class="text-white font-bold no-underline hover:text-primary hover:scale-110 transition-all"
-      exact-active-class="text-primary"
-      v-slot="{ isActive }"
-    >
-      <span :class="{ 'text-primary': isActive }">Contact</span>
-    </router-link>
+    
+    <div class="flex flex-wrap justify-center sm:justify-end gap-4 sm:gap-8">
+      <router-link
+        to="/portfolio"
+        class="text-white font-bold no-underline hover:text-primary hover:scale-110 transition-all"
+        exact-active-class="text-primary"
+        v-slot="{ isActive }"
+      >
+        <span :class="{ 'text-primary': isActive }">Projets</span>
+      </router-link>
+      <router-link
+        to="/competences"
+        class="text-white font-bold no-underline hover:text-primary hover:scale-110 transition-all"
+        exact-active-class="text-primary"
+        v-slot="{ isActive }"
+      >
+        <span :class="{ 'text-primary': isActive }">Compétences</span>
+      </router-link>
+      <router-link
+        to="/cv"
+        class="text-white font-bold no-underline hover:text-primary hover:scale-110 transition-all"
+        exact-active-class="text-primary"
+        v-slot="{ isActive }"
+      >
+        <span :class="{ 'text-primary': isActive }">CV</span>
+      </router-link>
+      <router-link
+        to="/contact"
+        class="text-white font-bold no-underline hover:text-primary hover:scale-110 transition-all"
+        exact-active-class="text-primary"
+        v-slot="{ isActive }"
+      >
+        <span :class="{ 'text-primary': isActive }">Contact</span>
+      </router-link>
+    </div>
   </nav>
 </template>
 
